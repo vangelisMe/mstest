26288de1ec03127cbf380d98c018236fee0c5763
vangelisMe <vangelis@methenitis.de>
2020-02-09 13:35:50 +0100

    Added Controller for User

---
+++ src/main/java/de/test/mstest/controller/get/UserControllerGet.java
@@ -0,0 +1,14 @@
+package de.test.mstest.controller.get;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserControllerGet {
+
+    @GetMapping("/users/{id}")
+    public void getUser(@PathVariable Integer id) {
+        System.out.println("ID: " + id);
+    }
+}
